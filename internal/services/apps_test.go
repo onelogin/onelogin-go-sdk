84b8962537e678d917bd818d716b0a693397a07d
Valeriy Sosnovskiy <valeriysosnovskiy@OL3344-admins-MacBook-Pro.local>
2019-10-20 17:15:14 -0700

    Updated delete tests

--- internal/services/apps_test.go
+++ internal/services/apps_test.go
@@ -26,7 +26,7 @@ func (auth *TypeAuthV2Mock) Authorize() (*http.Response, *models.AuthResp, error
 	return args.Get(0).(*http.Response), args.Get(1).(*models.AuthResp), args.Error(2)
 }
 
-func TestGetAppById(t *testing.T) {
+func TestGetAppByID(t *testing.T) {
 	tests := map[string]struct {
 		authMockAuthToken      string
 		authMockErr            error
@@ -119,11 +119,110 @@ func TestGetAppById(t *testing.T) {
 			resultResp, resultPayload, resultErr := apps.GetAppByID(test.appMockPayload.ID)
 
 			if test.isAppErrExpected {
+				assert.NotNil(t, resultErr)
 				assert.Equal(t, test.expectedAppErrorMsg, resultErr.Error())
 			} else {
+				assert.Nil(t, resultErr)
 				assert.Equal(t, test.expectedAppStatusCode, resultResp.StatusCode)
 				assert.Equal(t, test.expectedAppRespPayload.ID, resultPayload.ID)
 				assert.Equal(t, test.expectedAppRespPayload.ConnectorID, resultPayload.ConnectorID)
+				assert.Nil(t, resultErr)
+			}
+
+			// make sure the mocked function was called
+			mockedObj.AssertExpectations(t)
+		})
+	}
+}
+
+func TestDeleteAppByID(t *testing.T) {
+	tests := map[string]struct {
+		authMockAuthToken     string
+		authMockErr           error
+		authMockStatusCode    int
+		appIDtoDelete         int32
+		appMockStatusCodeResp int
+		isAppErrExpected      bool
+		expectedAppErrorMsg   string
+		expectedAppStatusCode int
+	}{
+		"auth returns access token and successful deletion": {
+			authMockAuthToken:     "testAuthToken",
+			authMockErr:           nil,
+			authMockStatusCode:    http.StatusOK,
+			appIDtoDelete:         1234,
+			appMockStatusCodeResp: http.StatusOK,
+			isAppErrExpected:      false,
+			expectedAppErrorMsg:   "",
+			expectedAppStatusCode: http.StatusOK,
+		},
+		"auth returns access token and unsuccessful deletion w/ status code 404": {
+			authMockAuthToken:     "testAuthToken",
+			authMockErr:           nil,
+			authMockStatusCode:    http.StatusOK,
+			appIDtoDelete:         1234,
+			appMockStatusCodeResp: http.StatusNotFound,
+			isAppErrExpected:      true,
+			expectedAppErrorMsg:   "request error: context: apps v2 service, status_code: [404], error_message: Not Found",
+		},
+		"auth returns http.StatusUnauthorized status code": {
+			authMockAuthToken:     "testAuthToken",
+			authMockErr:           errors.New("test"),
+			authMockStatusCode:    http.StatusUnauthorized,
+			appIDtoDelete:         1234,
+			appMockStatusCodeResp: http.StatusNotFound,
+			isAppErrExpected:      true,
+			expectedAppErrorMsg:   "request error: context: apps v2 service, status_code: [401], error_message: test",
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			// set up mock auth response
+			authOutput, _ := json.Marshal(&models.AuthResp{
+				AccessToken: test.authMockAuthToken,
+			})
+
+			authResp := &http.Response{
+				StatusCode: test.authMockStatusCode,
+				Body:       ioutil.NopCloser(bytes.NewReader(authOutput)),
+			}
+
+			authErr := test.authMockErr
+
+			payloadRes := &models.AuthResp{
+				AccessToken: test.authMockAuthToken,
+			}
+
+			// set up the mock server
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(test.appMockStatusCodeResp)
+			}))
+
+			defer ts.Close()
+
+			client := &http.Client{
+				Timeout: time.Second * 5,
+			}
+
+			mockedObj := &TypeAuthV2Mock{}
+
+			mockedObj.On("Authorize", mock.Anything).Return(authResp, payloadRes, authErr)
+
+			apps := NewAppsV2(&AppsV2Config{
+				BaseURL: ts.URL,
+				Client:  client,
+				Auth:    mockedObj,
+			})
+
+			resultResp, resultErr := apps.DeleteApp(test.appIDtoDelete)
+
+			if test.isAppErrExpected {
+				assert.NotNil(t, resultErr)
+				assert.Equal(t, test.expectedAppErrorMsg, resultErr.Error())
+			} else {
+				assert.Nil(t, resultErr)
+				assert.Equal(t, test.expectedAppStatusCode, resultResp.StatusCode)
 				assert.Nil(t, resultErr)
 			}
 

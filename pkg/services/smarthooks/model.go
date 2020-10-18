7efce02dbccbef32c9c93778e74f3db344021741
Dominick Caponi <d.caponi1@gmail.com>
2020-10-18 11:22:38 -0700

    adds smarthook resource

---
+++ pkg/services/smarthooks/model.go
@@ -0,0 +1,28 @@
+package smarthooks
+
+import "time"
+
+// SmartHookQuery represents available query parameters
+type SmartHookQuery struct {
+	Limit  string
+	Page   string
+	Cursor string
+	Type   string
+}
+
+// SmartHook represents a OneLogin SmartHook
+type SmartHook struct {
+	ID              *int32            `json:"id,omitempty"`
+	Type            *string           `json:"type,omitempty"`
+	Status          *string           `json:"status,omitempty"`
+	Disabled        *bool             `json:"disabled,omitempty"`
+	Retries         *int32            `json:"retires,omitempty"`
+	Timeout         *int32            `json:"timeout,omitempty"`
+	RiskEnabled     *bool             `json:"risk_enabled,omitempty"`
+	LocationEnabled *bool             `json:"location_enabled,omitempty"`
+	EnvVars         []string          `json:"env_vars,omitempty"`
+	Packages        map[string]string `json:"packages,omitempty"`
+	CreatedAt       time.Time         `json:"created_at,omitempty"`
+	UpdatedAt       time.Time         `json:"updated_at,omitempty"`
+	Function        *string           `json:"function,omitempty"` // this is the field that gets sent to the API as function
+}
---
+++ pkg/services/smarthooks/v1.go
@@ -0,0 +1,141 @@
+package smarthooks
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/onelogin/onelogin-go-sdk/pkg/services"
+	"github.com/onelogin/onelogin-go-sdk/pkg/services/olhttp"
+	"github.com/onelogin/onelogin-go-sdk/pkg/utils"
+)
+
+const errSmartHooksV2Context = "smarthooks v1 service"
+
+// V1Service holds the information needed to interface with a repository
+type V1Service struct {
+	Endpoint, ErrorContext string
+	Repository             services.Repository
+}
+
+// New creates the new svc service v2.
+func New(repo services.Repository, host string) *V1Service {
+	// ugly hack, remove after eks migration is done
+	if host == "https://oapi.onelogin-shadow01.com" {
+		host = "https://oapi-eks.onelogin-shadow01.com"
+	}
+	return &V1Service{
+		Endpoint:     fmt.Sprintf("%s/api/2/hooks", host),
+		Repository:   repo,
+		ErrorContext: errSmartHooksV2Context,
+	}
+}
+
+// Query retrieves all the smarthooks from the repository that meet the query criteria passed in the
+// request payload. If an empty payload is given, it will retrieve all smarthooks
+func (svc *V1Service) Query(query *SmartHookQuery) ([]SmartHook, error) {
+	resp, err := svc.Repository.Read(olhttp.OLHTTPRequest{
+		URL:        svc.Endpoint,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		AuthMethod: "bearer",
+		Payload:    query,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var smarthooks []SmartHook
+	json.Unmarshal(resp, &smarthooks)
+	decodedSmarthooks := make([]SmartHook, len(smarthooks))
+	for i, smarthook := range smarthooks {
+		decodeFunction(&smarthook)
+		decodedSmarthooks[i] = smarthook
+	}
+	return decodedSmarthooks, nil
+}
+
+// GetOne retrieves the smarthook by id and returns it
+func (svc *V1Service) GetOne(id int32) (*SmartHook, error) {
+	resp, err := svc.Repository.Read(olhttp.OLHTTPRequest{
+		URL:        fmt.Sprintf("%s/%d", svc.Endpoint, id),
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		AuthMethod: "bearer",
+	})
+	if err != nil {
+		return nil, err
+	}
+	var smarthook SmartHook
+	json.Unmarshal(resp, &smarthook)
+	decodeFunction(&smarthook)
+	return &smarthook, nil
+}
+
+// Create takes a smarthook without an id and attempts to use the parameters to create it
+// in the API. Modifies the smarthook in place, or returns an error if one occurs
+func (svc *V1Service) Create(smarthook *SmartHook) error {
+	if encErr := encodeFunction(smarthook); encErr != nil {
+		return encErr
+	}
+	resp, err := svc.Repository.Create(olhttp.OLHTTPRequest{
+		URL:        svc.Endpoint,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		AuthMethod: "bearer",
+		Payload:    smarthook,
+	})
+	if err != nil {
+		return err
+	}
+	json.Unmarshal(resp, smarthook)
+	return nil
+}
+
+// Update takes a smarthook and an id and attempts to use the parameters to update it
+// in the API. Modifies the smarthook in place, or returns an error if one occurs
+func (svc *V1Service) Update(smarthook *SmartHook) error {
+	if smarthook.ID == nil {
+		return errors.New("No ID Given")
+	}
+	if encErr := encodeFunction(smarthook); encErr != nil {
+		return encErr
+	}
+	resp, err := svc.Repository.Update(olhttp.OLHTTPRequest{
+		URL:        fmt.Sprintf("%s/%d", svc.Endpoint, *smarthook.ID),
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		AuthMethod: "bearer",
+		Payload:    smarthook,
+	})
+	if err != nil {
+		return err
+	}
+	json.Unmarshal(resp, smarthook)
+	return nil
+}
+
+// Destroy deletes the smarthook with the given id, and if successful, it returns nil
+func (svc *V1Service) Destroy(id int32) error {
+	if _, err := svc.Repository.Destroy(olhttp.OLHTTPRequest{
+		URL:        fmt.Sprintf("%s/%d", svc.Endpoint, id),
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		AuthMethod: "bearer",
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func encodeFunction(smarthook *SmartHook) error {
+	if smarthook.Function == nil {
+		return errors.New("No Function Definition Given")
+	}
+	str := utils.EncodeString(*smarthook.Function)
+	smarthook.Function = &str
+	return nil
+}
+
+func decodeFunction(smarthook *SmartHook) error {
+	if smarthook.Function == nil {
+		return errors.New("No Function Definition Given")
+	}
+	str := utils.DecodeString(*smarthook.Function)
+	smarthook.Function = &str
+	return nil
+}

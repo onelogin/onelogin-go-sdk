266590e5e6cfd817e1c85d69ed6dda2b58a93c6a
Dominick Caponi <d.caponi1@gmail.com>
2020-10-20 16:32:42 -0700

    removes packages field from smarthooks

--- pkg/services/smarthooks/model.go
+++ pkg/services/smarthooks/model.go
@@ -12,17 +12,16 @@ type SmartHookQuery struct {
 
 // SmartHook represents a OneLogin SmartHook
 type SmartHook struct {
-	ID              *string           `json:"id,omitempty"`
-	Type            *string           `json:"type,omitempty"`
-	Status          *string           `json:"status,omitempty"`
-	Disabled        *bool             `json:"disabled,omitempty"`
-	Retries         *int32            `json:"retries,omitempty"`
-	Timeout         *int32            `json:"timeout,omitempty"`
-	RiskEnabled     *bool             `json:"risk_enabled,omitempty"`
-	LocationEnabled *bool             `json:"location_enabled,omitempty"`
-	EnvVars         []string          `json:"env_vars,omitempty"`
-	Packages        map[string]string `json:"packages,omitempty"`
-	CreatedAt       *time.Time        `json:"created_at,omitempty"`
-	UpdatedAt       *time.Time        `json:"updated_at,omitempty"`
-	Function        *string           `json:"function,omitempty"` // this is the field that gets sent to the API as function
+	ID              *string    `json:"id,omitempty"`
+	Type            *string    `json:"type,omitempty"`
+	Status          *string    `json:"status,omitempty"`
+	Disabled        *bool      `json:"disabled,omitempty"`
+	Retries         *int32     `json:"retries,omitempty"`
+	Timeout         *int32     `json:"timeout,omitempty"`
+	RiskEnabled     *bool      `json:"risk_enabled,omitempty"`
+	LocationEnabled *bool      `json:"location_enabled,omitempty"`
+	EnvVars         []string   `json:"env_vars,omitempty"`
+	CreatedAt       *time.Time `json:"created_at,omitempty"`
+	UpdatedAt       *time.Time `json:"updated_at,omitempty"`
+	Function        *string    `json:"function,omitempty"` // this is the field that gets sent to the API as function
 }

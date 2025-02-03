b73959e1514f52854e5d72fe2601543e8389a9ee
sparshgirdhar <sparsh.girdhar@voxco.com>
2025-02-03 20:54:32 +0530

    Updated the Apps and App Rules APIs

--- pkg/onelogin/apps.go
+++ pkg/onelogin/apps.go
@@ -19,10 +19,9 @@ func (sdk *OneloginSDK) CreateApp(app mod.App) (interface{}, error) {
 		return nil, err
 	}
 	return utl.CheckHTTPResponse(resp)
-
 }
 
-// was ListApps
+// List Apps
 func (sdk *OneloginSDK) GetApps(queryParams mod.Queryable) (interface{}, error) {
 	p, err := utl.BuildAPIPath(AppPath)
 	if err != nil {
@@ -35,6 +34,7 @@ func (sdk *OneloginSDK) GetApps(queryParams mod.Queryable) (interface{}, error) 
 	return utl.CheckHTTPResponse(resp)
 }
 
+// Get an App
 func (sdk *OneloginSDK) GetAppByID(id int, queryParams mod.Queryable) (interface{}, error) {
 	p, err := utl.BuildAPIPath(AppPath, id)
 	if err != nil {
@@ -45,7 +45,6 @@ func (sdk *OneloginSDK) GetAppByID(id int, queryParams mod.Queryable) (interface
 		return nil, err
 	}
 	return utl.CheckHTTPResponse(resp)
-
 }
 
 func (sdk *OneloginSDK) UpdateApp(id int, app mod.App) (interface{}, error) {
@@ -58,11 +57,11 @@ func (sdk *OneloginSDK) UpdateApp(id int, app mod.App) (interface{}, error) {
 		return nil, err
 	}
 	return utl.CheckHTTPResponse(resp)
-
 }
 
-func (sdk *OneloginSDK) DeleteApp(id int) (interface{}, error) {
-	p, err := utl.BuildAPIPath(AppPath, id)
+// Delete an App Parameter
+func (sdk *OneloginSDK) DeleteAppParameter(id int, parameterID int) (interface{}, error) {
+	p, err := utl.BuildAPIPath(AppPath, id, "parameters", parameterID)
 	if err != nil {
 		return nil, err
 	}
@@ -71,63 +70,10 @@ func (sdk *OneloginSDK) DeleteApp(id int) (interface{}, error) {
 		return nil, err
 	}
 	return utl.CheckHTTPResponse(resp)
-
 }
 
-func (sdk *OneloginSDK) CreateAppRule(id int, appRule mod.AppRule) (interface{}, error) {
+func (sdk *OneloginSDK) DeleteApp(id int) (interface{}, error) {
 	p, err := utl.BuildAPIPath(AppPath, id)
-	if err != nil {
-		return nil, err
-	}
-	resp, err := sdk.Client.Post(&p, appRule)
-	if err != nil {
-		return nil, err
-	}
-	return utl.CheckHTTPResponse(resp)
-
-}
-
-func (sdk *OneloginSDK) GetAppRules(id int, queryParams mod.Queryable) (interface{}, error) {
-	p, err := utl.BuildAPIPath(AppPath, id)
-	if err != nil {
-		return nil, err
-	}
-	resp, err := sdk.Client.Get(&p, queryParams)
-	if err != nil {
-		return nil, err
-	}
-	return utl.CheckHTTPResponse(resp)
-
-}
-
-func (sdk *OneloginSDK) GetAppRuleByID(id, ruleID int, queryParams mod.Queryable) (interface{}, error) {
-	p, err := utl.BuildAPIPath(AppPath, id)
-	if err != nil {
-		return nil, err
-	}
-	resp, err := sdk.Client.Get(&p, queryParams)
-	if err != nil {
-		return nil, err
-	}
-	return utl.CheckHTTPResponse(resp)
-
-}
-
-func (sdk *OneloginSDK) UpdateAppRule(id, ruleID int, appRule mod.AppRule, queryParams map[string]string) (interface{}, error) {
-	p, err := utl.BuildAPIPath(AppPath, id, "rules", ruleID)
-	if err != nil {
-		return nil, err
-	}
-	resp, err := sdk.Client.Put(&p, appRule)
-	if err != nil {
-		return nil, err
-	}
-	return utl.CheckHTTPResponse(resp)
-
-}
-
-func (sdk *OneloginSDK) DeleteAppRule(id, ruleID int, queryParams map[string]string) (interface{}, error) {
-	p, err := utl.BuildAPIPath(AppPath, id, "rules", ruleID)
 	if err != nil {
 		return nil, err
 	}
@@ -144,6 +90,141 @@ func (sdk *OneloginSDK) GetAppUsers(appID int) (interface{}, error) {
 		return nil, err
 	}
 	resp, err := sdk.Client.Get(&p, nil)
+	if err != nil {
+		return nil, err
+	}
+	return utl.CheckHTTPResponse(resp)
+}
+
+// App Rules APIs
+// list all rules for an app
+func (sdk *OneloginSDK) GetAppRules(appID int, queryParams mod.Queryable) (interface{}, error) {
+	p, err := utl.BuildAPIPath(AppPath, appID, "rules")
+	if err != nil {
+		return nil, err
+	}
+	resp, err := sdk.Client.Get(&p, queryParams)
+	if err != nil {
+		return nil, err
+	}
+	return utl.CheckHTTPResponse(resp)
+}
+
+// get rule by ruleId
+func (sdk *OneloginSDK) GetAppRuleByID(appID int, ruleID int, queryParams mod.Queryable) (interface{}, error) {
+	p, err := utl.BuildAPIPath(AppPath, appID, "rules", ruleID)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := sdk.Client.Get(&p, queryParams)
+	if err != nil {
+		return nil, err
+	}
+	return utl.CheckHTTPResponse(resp)
+}
+
+func (sdk *OneloginSDK) CreateAppRule(appID int, appRule mod.AppRule) (interface{}, error) {
+	p, err := utl.BuildAPIPath(AppPath, appID, "rules")
+	if err != nil {
+		return nil, err
+	}
+	resp, err := sdk.Client.Post(&p, appRule)
+	if err != nil {
+		return nil, err
+	}
+	return utl.CheckHTTPResponse(resp)
+}
+
+func (sdk *OneloginSDK) UpdateAppRule(appID, ruleID int, appRule mod.AppRule, queryParams map[string]string) (interface{}, error) {
+	p, err := utl.BuildAPIPath(AppPath, appID, "rules", ruleID)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := sdk.Client.Put(&p, appRule)
+	if err != nil {
+		return nil, err
+	}
+	return utl.CheckHTTPResponse(resp)
+}
+
+func (sdk *OneloginSDK) DeleteAppRule(appID, ruleID int, queryParams map[string]string) (interface{}, error) {
+	p, err := utl.BuildAPIPath(AppPath, appID, "rules", ruleID)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := sdk.Client.Delete(&p)
+	if err != nil {
+		return nil, err
+	}
+	return utl.CheckHTTPResponse(resp)
+}
+
+func (sdk *OneloginSDK) ListAppRulesConditions(appID int) (interface{}, error) {
+	p, err := utl.BuildAPIPath(AppPath, appID, "rules", "conditions")
+	if err != nil {
+		return nil, err
+	}
+	resp, err := sdk.Client.Get(&p, nil)
+	if err != nil {
+		return nil, err
+	}
+	return utl.CheckHTTPResponse(resp)
+}
+
+func (sdk *OneloginSDK) GetAppRuleOperators(appID int, ruleConditionValue string, queryParams mod.Queryable) (interface{}, error) {
+	p, err := utl.BuildAPIPath(AppPath, appID, "rules", "conditions", ruleConditionValue, "operators")
+	if err != nil {
+		return nil, err
+	}
+	resp, err := sdk.Client.Get(&p, queryParams)
+	if err != nil {
+		return nil, err
+	}
+	return utl.CheckHTTPResponse(resp)
+}
+
+func (sdk *OneloginSDK) GetAppRuleConditionValues(appId int, conditionValue string) (interface{}, error) {
+	p, err := utl.BuildAPIPath(AppPath, appId, "rules", "conditions", conditionValue, "values")
+	if err != nil {
+		return nil, err
+	}
+	resp, err := sdk.Client.Get(&p, nil)
+	if err != nil {
+		return nil, err
+	}
+	return utl.CheckHTTPResponse(resp)
+}
+
+func (sdk *OneloginSDK) GetAppRuleActions(appId int) (interface{}, error) {
+	p, err := utl.BuildAPIPath(AppPath, appId, "rules", "actions")
+	if err != nil {
+		return nil, err
+	}
+	resp, err := sdk.Client.Get(&p, nil)
+	if err != nil {
+		return nil, err
+	}
+	return utl.CheckHTTPResponse(resp)
+}
+
+func (sdk *OneloginSDK) ListAppRulesActionValues(appId int, actionValue string) (interface{}, error) {
+	p, err := utl.BuildAPIPath(AppPath, appId, "rules", "actions", actionValue, "values")
+	if err != nil {
+		return nil, err
+	}
+	resp, err := sdk.Client.Get(&p, nil)
+	if err != nil {
+		return nil, err
+	}
+	return utl.CheckHTTPResponse(resp)
+}
+
+func (sdk *OneloginSDK) BulkSortAppRules(appId int, ruleIDs []int) (interface{}, error) {
+	p, err := utl.BuildAPIPath(AppPath, appId, "rules", "sort")
+	if err != nil {
+		return nil, err
+	}
+	resp, err := sdk.Client.Put(&p, ruleIDs)
 	if err != nil {
 		return nil, err
 	}
